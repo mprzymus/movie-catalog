28b1d93f65ec44b29bab4d02b392e3a740dfa9c2
mprzymus <przymus@gmail.com>
2021-05-20 11:42:36 +0200

    init project

---
+++ movie-cat/src/test/java/pl/przymuslogisz/moviecat/MovieCatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.przymuslogisz.moviecat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
