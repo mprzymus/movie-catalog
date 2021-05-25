ca95efbe2c1aae76e3a4a4f8177f277889543afb
mprzymus <przymus@gmail.com>
2021-05-25 22:13:05 +0200

    empty controllers classes

---
+++ movie-cat/src/main/java/pl/przymuslogisz/moviecat/controllers/MovieController.java
@@ -0,0 +1,11 @@
+package pl.przymuslogisz.moviecat.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/movie")
+@RequiredArgsConstructor
+public class MovieController {
+}
